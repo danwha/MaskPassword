abb917117edc60bc68fd1efcd08ac45cbfbcb739
danwha <74486531+danwha@users.noreply.github.com>
2021-01-19 10:25:51 +0900

    Add files via upload

--- examples/main.js
+++ examples/main.js
@@ -20,18 +20,13 @@ const validRegExp   = /^[가-힣A-Za-z0-9-!:^_'?,.=\s+]{1,200}$/
 const invalidRegExp = `/^[가-A-Za-z0-9-!:^_'?,.=\s+]{1,200}$/`
 const setRegExp = reg => {
   var isValid = true;
-  try {
-    new RegExp(reg)
-  } catch(e) {
-    isValid = false;
-  }
-  if(isValid){
-    let Mask = new MaskPassword()
-    Mask.setRegularExpression(reg)
-  }
+  try {new RegExp(reg)} catch(e) {isValid = false}
+  if(isValid) MaskPassword.setRegularExpression(reg)
   return isValid
 }
 console.log(__line, setRegExp(validRegExp))
+
+MaskPassword.setLocale(9) // korean
 
 let ValidMask = new MaskPassword()
 ValidMask.pushMonth2()
@@ -47,8 +42,7 @@ InvalidMask.pushDate()
 InvalidMask.pushHour()
 console.log(__line, 'InvalidStatement syntax', InvalidMask)
 
-console.log(__line, ValidMask.getRegularExpression())
-console.log(__line, InvalidMask.getRegularExpression())
+console.log(__line, MaskPassword.getRegularExpression())
 
 console.log('Valid', ValidMask.isValid())
 console.log('Invalid', InvalidMask.isValid())
@@ -82,4 +76,3 @@ let validPasswordStruct = [
 let validPassword = validPasswordStruct.join('')
 let ruleStr2 = ValidMask.decryption(storage, validPassword)
 console.log(__line, ruleStr2)
-
